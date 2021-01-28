52bfab41ca9b5a642fd3e4c8da8a5bc89ab6b45c
MarcosVel <marcos.veloso@sempreceub.com>
2021-01-28 00:41:53 -0300

    Criando API

--- src/screens/SignUp/index.js
+++ src/screens/SignUp/index.js
@@ -1,11 +1,79 @@
-import React from 'react';
-import { Text } from 'react-native';
-import { Container } from './styles';
+import React, { useState } from 'react';
+import { useNavigation } from '@react-navigation/native';
+import {
+  Container,
+  InputArea,
+  CustomButton,
+  CustomButtonText,
+  ViewSignMessageButton,
+  SignMessageButton,
+  SignMessageButtonText,
+  SignMessageButtonTextBold,
+} from './styles';
+
+import SignInput from '../../components/SignInput';
+
+import BarberLogo from "../../assets/barber.svg";
+import PersonIcon from "../../assets/person.svg";
+import EmailIcon from "../../assets/email.svg";
+import LockIcon from "../../assets/lock.svg";
 
 export default () => {
+  const navigation = useNavigation();
+
+  const [ nameField, setNameField ] = useState('');
+  const [ emailField, setEmailField ] = useState('');
+  const [ passwordField, setPasswordField ] = useState('');
+
+  const handleSignClick = () => {
+  }
+
+  const handleMessageButtonClick = () => {
+    navigation.reset({
+      routes: [ { name: 'SignIn' } ]
+    });
+  }
+
   return (
     <Container>
-      <Text>SignUp</Text>
+      <BarberLogo width="100%" height='160' />
+
+      <InputArea>
+        <SignInput
+          IconSvg={ PersonIcon }
+          placeholder="Digite seu nome"
+          value={ nameField }
+          onChangeText={ t => setNameField(t) }
+        />
+
+        <SignInput
+          IconSvg={ EmailIcon }
+          placeholder="Digite seu e-mail"
+          value={ emailField }
+          onChangeText={ t => setEmailField(t) }
+        />
+
+        <SignInput
+          IconSvg={ LockIcon }
+          placeholder="Digite sua senha"
+          value={ passwordField }
+          onChangeText={ t => setPasswordField(t) }
+          password={ true }
+        />
+
+        <CustomButton onPress={ handleSignClick }>
+          <CustomButtonText>CADASTRAR</CustomButtonText>
+        </CustomButton>
+
+      </InputArea>
+
+      <ViewSignMessageButton>
+        <SignMessageButtonText>Já possui uma conta?</SignMessageButtonText>
+        <SignMessageButton onPress={ handleMessageButtonClick }>
+          <SignMessageButtonTextBold>Faça Login</SignMessageButtonTextBold>
+        </SignMessageButton>
+      </ViewSignMessageButton>
+
     </Container>
   );
 }
